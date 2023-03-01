7b0e05686cbb3915e6a84f25d2a7a0eb9c3c2fe5
cybertcode <cybertcode@gmail.com>
2023-03-01 16:18:49 -0500

    New proyect

---
+++ app/Http/Controllers/EnvoiceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Envoice;
+use App\Http\Requests\StoreEnvoiceRequest;
+use App\Http\Requests\UpdateEnvoiceRequest;
+
+class EnvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEnvoiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Envoice $envoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Envoice $envoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEnvoiceRequest $request, Envoice $envoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Envoice $envoice)
+    {
+        //
+    }
+}
