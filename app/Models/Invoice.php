1650fe8224428dd6008ee46615cee8dea5593f84
cybertcode <cybertcode@gmail.com>
2023-03-01 17:47:21 -0500

    Agregado relaciones

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
     use HasFactory;
+    // Asignación masiva
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['serie', 'correlative', 'base', 'igv', 'total', 'user_id'];
+    // Relación de uno a muchos inversa
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
