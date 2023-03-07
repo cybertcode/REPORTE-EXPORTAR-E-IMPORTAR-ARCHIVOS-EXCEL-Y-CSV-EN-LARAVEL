3433d7449106d3b542dd093d5d19b1cdbe1aaa91
cybertcode <cybertcode@gmail.com>
2023-03-07 17:51:12 -0500

    Estilos - Dar formato a las fechas

--- app/Exports/InvoiceExport.php
+++ app/Exports/InvoiceExport.php
@@ -6,10 +6,13 @@ use App\Models\Invoice;
 use Illuminate\Contracts\Support\Responsable;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithCustomStartCell;
+use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Excel;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class InvoiceExport implements FromCollection, WithCustomStartCell, Responsable
+class InvoiceExport implements FromCollection, WithCustomStartCell, Responsable, WithMapping, WithColumnFormatting
 {
     use Exportable;
     private $filters;
@@ -24,9 +27,31 @@ class InvoiceExport implements FromCollection, WithCustomStartCell, Responsable
     {
         return Invoice::filter($this->filters)->get();
     }
-    // ES imprescindible definir este método para evitar error
+    // ES imprescindible definir este método para evitar error DE WithCustomStartCell
     public function startCell(): string
     {
         return 'A5';
     }
+    // ES imprescindible definir este método para evitar error de WithMapping
+    public function map($invoice): array
+    {
+        return [
+            $invoice->serie,
+            $invoice->correlative,
+            $invoice->base,
+            $invoice->igv,
+            $invoice->total,
+            $invoice->user->name,
+            Date::dateTimeToExcel($invoice->created_at),
+        ];
+    }
+    // ES imprescindible definir este método para evitar error de WithColumnFormatting
+    public function columnFormats(): array
+    {
+        // G => Es la columna del excel a formaterar
+        return [
+            'G' => 'dd/mm/yyyy',
+        ];
+    }
+
 }
